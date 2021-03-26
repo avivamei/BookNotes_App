17a0ee1a6bd518771675ba39a0340306ab5f0c79
avivamei <aviivameii@gmail.com>
2021-03-26 13:53:59 -0700

    implemented main gui and parts of book panel

---
+++ src/main/ui/BookPanel.java
@@ -0,0 +1,64 @@
+package ui;
+
+import model.Book;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class BookPanel extends JPanel implements ActionListener {
+    private Book book;
+    private GUI gui;
+
+    public BookPanel(Book book, GUI gui) {
+        this.book = book;
+        this.gui = gui;
+        initializeGraphics();
+    }
+
+    private void initializeGraphics() {
+        JLabel bookTitle = new JLabel(book.getTitle());
+        add(bookTitle, BorderLayout.LINE_START);
+
+        JPanel bookContent = setUpContent();
+        add(bookContent, BorderLayout.CENTER);
+
+        JPanel bookOptions = setUpOptions();
+        add(bookOptions, BorderLayout.LINE_END);
+
+    }
+
+    private JPanel setUpOptions() {
+        JPanel optionsPanel = new JPanel();
+        optionsPanel.setLayout(new GridLayout(2, 1));
+
+        JButton selectBook = new JButton("Select");
+        selectBook.setActionCommand("selectBookButton");
+        selectBook.addActionListener(this);
+
+        JButton removeBook = new JButton("Remove");
+        removeBook.setActionCommand("removeBookButton");
+        removeBook.addActionListener(this);
+
+        optionsPanel.add(selectBook);
+        optionsPanel.add(removeBook);
+        return optionsPanel;
+    }
+
+    private JPanel setUpContent() {
+        JPanel contentPanel = new JPanel();
+        contentPanel.setLayout(new GridLayout(2, 1));
+        JLabel author = new JLabel("Author: " + book.getAuthor());
+        JLabel genre = new JLabel("Genre: " + book.getGenre());
+        contentPanel.add(author);
+        contentPanel.add(genre);
+        return contentPanel;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
---
+++ src/main/ui/GUI.java
@@ -0,0 +1,107 @@
+package ui;
+
+import model.Book;
+import model.Bookshelf;
+import persistence.JsonReader;
+import persistence.JsonWriter;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class GUI extends JFrame implements ActionListener {
+    private Bookshelf bookshelf;
+
+    public GUI() {
+        super("Bookshelf");
+        initializeGraphics();
+    }
+
+    public static void main(String[] args) {
+        new GUI();
+    }
+
+    private void initializeGraphics() {
+        bookshelf = new Bookshelf("Bookshelf");
+
+        setLayout(new BorderLayout());
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        add(bookshelfHeaderPanel(), BorderLayout.PAGE_START);
+        add(bookshelfContentPanel(), BorderLayout.CENTER);
+        add(bookshelfFooterPanel(), BorderLayout.PAGE_END);
+
+        pack();
+        setVisible(true);
+    }
+
+    private JPanel bookshelfHeaderPanel() {
+        JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+
+        JLabel name = new JLabel();
+        name.setText(bookshelf.getName());
+        panel.add(name, BorderLayout.PAGE_START);
+
+        JButton addBook = new JButton("Add Book");
+        addBook.setActionCommand("addBookButton");
+        addBook.addActionListener(this);
+        panel.add(addBook, BorderLayout.CENTER);
+
+        return panel;
+    }
+
+    private JPanel bookshelfContentPanel() {
+        JPanel panel = new JPanel();
+        for (Book b : bookshelf.getBookList()) {
+            BookPanel bookPanel = new BookPanel(b, this);
+            panel.add(bookPanel);
+        }
+        return panel;
+    }
+
+    private JPanel bookshelfFooterPanel() {
+        JPanel panel = new JPanel();
+
+        JButton load = new JButton("Load");
+        load.setActionCommand("loadButton");
+        load.addActionListener(this);
+
+        JButton save = new JButton("Save");
+        save.setActionCommand("saveButton");
+        save.addActionListener(this);
+
+        panel.add(load);
+        panel.add(save);
+
+        return panel;
+    }
+
+    @Override
+    //This is the method that is called when the the JButton btn is clicked
+    public void actionPerformed(ActionEvent e) {
+        if (e.getActionCommand().equals("loadButton")) {
+            JsonReader reader = new JsonReader("./data/bookshelf.json");
+            try {
+                reader.read();
+            } catch (IOException exception) {
+                exception.printStackTrace();
+            }
+        } else if (e.getActionCommand().equals("saveButton")) {
+            JsonWriter writer = new JsonWriter("./data/bookshelf.json");
+            try {
+                writer.open();
+            } catch (FileNotFoundException exception) {
+                exception.printStackTrace();
+            }
+            writer.write(bookshelf);
+            writer.close();
+        } else if (e.getActionCommand().equals("addBookButton")) {
+            new AddBookPopUp();
+        }
+    }
+
+}
